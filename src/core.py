6deee5b0775380c2e85f70fff06348431a68696f
GeneXu <genexu1117@gmail.com>
2017-03-14 23:13:55 +0800

    add basic frame container

--- src/core.py
+++ src/core.py
@@ -1,4 +1,14 @@
 import urwid
 
 def run():
-    print 'run'
+    header = urwid.Text(u"Header", align='center')
+
+    body_txt = urwid.Text(u"Body", align='center')
+    body = urwid.Filler(body_txt, valign='middle')
+
+    footer = urwid.Text(u"Footer", align='center')
+
+    frame = urwid.Frame(body, header=header, footer=footer, focus_part='body')
+
+    loop = urwid.MainLoop(frame)
+    loop.run()
