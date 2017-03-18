11ede1b555eb1e72340073291794c862590e8ae0
GeneXu <genexu1117@gmail.com>
2017-03-18 21:36:44 +0800

    redefine bundle content widget

--- src/bundle.py
+++ src/bundle.py
@@ -3,17 +3,15 @@ from header import Header
 from body import Body
 from footer import Footer
 
-header = Header()
-body = Body()
-footer = Footer()
-
 class Bundle(object):
     """docstring for Bundle."""
     def __init__(self):
         super(Bundle, self).__init__()
-
+        self.header = Header()
+        self.body = Body()
+        self.footer = Footer()
     def render(self):
-        return urwid.Frame(body.render(), header=header.render(), footer=footer.render(), focus_part='body')
+        return urwid.Frame(self.body.render(), header=self.header.render(), footer=self.footer.render(), focus_part='body')
 
     def globle_input_listener(self, key):
-        footer.input_response(key)
+        self.footer.input_response(key)
