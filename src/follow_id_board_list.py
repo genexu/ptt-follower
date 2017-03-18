7b564a6f82d62746ed843e0cb57651a2f96813cd
GeneXu <genexu1117@gmail.com>
2017-03-18 22:24:59 +0800

    bind id list and board list to json file

--- src/follow_id_board_list.py
+++ src/follow_id_board_list.py
@@ -1,15 +1,23 @@
 import urwid
-
-test_list = ['test_board_0','test_board_1', 'test_board_2']
+import json
 
 class FollowIdBoardList(object):
     """Docstring For FollowIdBoardList."""
-    def __init__(self):
+    def __init__(self, id_index = 0, list_item = []):
         super(FollowIdBoardList, self).__init__()
+        self.id_index = id_index
+        self.list_item = list_item
+
+    def update_id_index(self, index):
+        self.id_index = index
 
     def render(self):
+        with open('data.json') as data_file:
+            data_loaded = json.load(data_file)
+            self.list_item = data_loaded['follow'][self.id_index]['board']
+
         board_list_item = [urwid.Text('Follow Id Board List'), urwid.Divider()]
-        for board in test_list:
+        for board in self.list_item:
             button = urwid.Button(board)
             board_list_item.append(button)
 
--- src/follow_id_list.py
+++ src/follow_id_list.py
@@ -1,15 +1,20 @@
 import urwid
-
-test_list = ['test_follow_id_0','test_follow_id_1', 'test_follow_id_2']
+import json
 
 class FollowIdList(object):
     """Docstring For Follow_Id_List."""
-    def __init__(self):
+    def __init__(self, list_item = []):
         super(FollowIdList, self).__init__()
+        self.list_item = list_item
 
     def render(self):
+        with open('data.json') as data_file:
+            data_loaded = json.load(data_file)
+            for f in data_loaded['follow']:
+                self.list_item.append(f['id'])
+
         id_list_item = [urwid.Text('Follow Id List'), urwid.Divider()]
-        for user_id in test_list:
+        for user_id in self.list_item:
             button = urwid.Button(user_id)
             id_list_item.append(button)
 
