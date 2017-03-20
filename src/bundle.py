6afa74b376e6a35da392d581d3fe1abd3add07dc
GeneXu <genexu1117@gmail.com>
2017-03-20 23:33:37 +0800

    add Add Id and Add Board status, handle status switch

--- src/bundle.py
+++ src/bundle.py
@@ -3,6 +3,10 @@ from header import Header
 from body import Body
 from footer import Footer
 
+NORMAL = 'NORMAL'
+ADD_NEW_ID = 'ADD_NEW_ID'
+ADD_NEW_BOARD = 'ADD_NEW_BOARD'
+
 class Bundle(object):
     """docstring for Bundle."""
     def __init__(self):
@@ -10,8 +14,29 @@ class Bundle(object):
         self.header = Header()
         self.body = Body()
         self.footer = Footer()
+        self.status = NORMAL
+        self.output = urwid.Frame(self.body.render(), header=self.header.render(), footer=self.footer.render(), focus_part='body')
+
     def render(self):
-        return urwid.Frame(self.body.render(), header=self.header.render(), footer=self.footer.render(), focus_part='body')
+        return self.output
 
     def globle_input_listener(self, key):
         self.footer.input_response(key)
+        if key in ('I'):
+            self.status = ADD_NEW_ID
+            self.footer.add_new_id()
+            self.output.focus_position = 'footer'
+
+        if key in ('B'):
+            self.status = ADD_NEW_BOARD
+            self.footer.add_new_board()
+            self.output.focus_position = 'footer'
+
+        if key in ('esc'):
+            if self.status in (ADD_NEW_ID, ADD_NEW_BOARD):
+                self.status = NORMAL
+                self.footer.desc()
+                self.output.focus_position = 'body'
+
+        if key in ('q', 'Q'):
+            raise urwid.ExitMainLoop()
--- src/footer.py
+++ src/footer.py
@@ -1,14 +1,41 @@
 import urwid
 
+DESC = 'DESC'
+ADD_NEW_ID = 'ADD_NEW_ID'
+ADD_NEW_BOARD = 'ADD_NEW_BOARD'
+
 class Footer(object):
     """Docstring for Footer."""
     def __init__(self, desc="Default Desc", input_response="None"):
         super(Footer, self).__init__()
         self.desc_text = urwid.Text(desc, align='left')
         self.input_response_text = urwid.Text('Response Input: %s' %(repr(input_response)), align='right')
+        self.output = urwid.Columns(self.generate(DESC))
 
-    def render(self):
-        return urwid.Columns([self.desc_text, self.input_response_text])
+    def generate(self, mode):
+        if mode == DESC:
+            return [ self.desc_text, self.input_response_text ]
+        if mode == ADD_NEW_ID:
+            return [urwid.Edit(u"Id: "), urwid.Text(u"(Esc) Exit Add Mode", align='right')]
+        if mode == ADD_NEW_BOARD:
+            return [urwid.Edit(u"Board: "), urwid.Text(u"(Esc) Exit Add Mode", align='right')]
+
+    def desc(self):
+        self.contents_replace(self.generate(DESC))
+
+    def add_new_id(self):
+        self.contents_replace(self.generate(ADD_NEW_ID))
+
+    def add_new_board(self):
+        self.contents_replace(self.generate(ADD_NEW_BOARD))
+
+    def contents_replace(self, contents):
+        self.output.contents = []
+        for c in contents:
+            self.output.contents.append((c, self.output.options()))
 
     def input_response(self, key):
         self.input_response_text.set_text('Response Input: %s' %(repr(key)))
+
+    def render(self):
+        return self.output
