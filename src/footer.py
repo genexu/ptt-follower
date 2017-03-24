b811640a472b8f6282ef312612d1f610593654a0
GeneXu <genexu1117@gmail.com>
2017-03-24 23:42:48 +0800

    modify footer default desc, modify post title, remove test posts

--- src/footer.py
+++ src/footer.py
@@ -3,10 +3,12 @@ import urwid
 DESC = 'DESC'
 ADD_NEW_ID = 'ADD_NEW_ID'
 ADD_NEW_BOARD = 'ADD_NEW_BOARD'
+DEFAULT_DESC = '(I)Add Id (B)Add Board (q/Q)Exit'
+DEFAULT_RESPONSE = 'None'
 
 class Footer(object):
     """Docstring for Footer."""
-    def __init__(self, desc="Default Desc", input_response="None"):
+    def __init__(self, desc=DEFAULT_DESC, input_response=DEFAULT_RESPONSE):
         super(Footer, self).__init__()
         self.desc_text = urwid.Text(desc, align='left')
         self.input_response_text = urwid.Text('Response Input: %s' %(repr(input_response)), align='right')
